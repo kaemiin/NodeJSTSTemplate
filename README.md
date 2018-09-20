3d808c8eb22312e8124427cb82ba2712a59acd73
kaemiin <kaemiin@xinosys.com>
2018-09-20 11:53:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# PatiscoXXX
+
+## Use Docker
+
+```
+docker run -it -v $(pwd):/workspace -v node_modules:/workspace/node_modules --name xxx kkarczmarczyk/node-yarn bash
+
+docker exec -it xxx bash
+```
+
+## Compile TypeScript
+
+```
+tsc
+```
---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('dotenv').config();
+
+const config = {
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectionLimit: process.env.DB_CONNECTIONLIMIT
+};
+
+//TODO
+console.log(config);
+
